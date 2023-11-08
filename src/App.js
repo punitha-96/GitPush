fe92a9a74340bbd552eddc55174a7f2ea01d5a75
punitha-96 <149648026+punitha-96@users.noreply.github.com>
2023-11-08 14:44:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttttt
+          Learn the React
         </a>
       </header>
     </div>
